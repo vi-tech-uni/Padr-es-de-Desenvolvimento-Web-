df2f87d6a77dfc46b00d64ecc6ad9a04655d7fe6
vi-tech-uni <273883@unifio.edu.br>
2026-08-19 01:41:27 +0000

    Configuração do JPA

---
+++ ecommerce-backend/src/main/java/br/edu/unifio/ecommerce_backend/entidades/Categoria/Categoria.java
@@ -0,0 +1,18 @@
+package br.edu.unifio.ecommerce_backend.entidades.Categoria;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Categoria {
+private int id;
+private String nome;
+private String descricao;
+
+
+
+}
---
+++ ecommerce-backend/src/main/java/br/edu/unifio/ecommerce_backend/entidades/Categoria/Java.java
@@ -0,0 +1,7 @@
+package br.edu.unifio.ecommerce_backend.entidades.Categoria;
+
+Integer
+int, double, 
+public class Categoria
+
+}
